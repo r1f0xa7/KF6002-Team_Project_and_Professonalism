391aba02d529684cfce5042caaaddc9b360c41d5
Md _ Rifat ( R!f0x@7 ) <81492392+r1f0xa7@users.noreply.github.com>
2024-08-22 22:25:46 +0100

    Delete event-management directory

--- event-management/views/404.php
+++
@@ -1,7 +0,0 @@
-<?php include(__DIR__.'/../includes/header.php'); ?>
-<div class="container">
-    <h2>404 - Page Not Found</h2>
-    <p>We're sorry, but the page you were looking for doesn't exist. Please check the URL or return to the homepage.</p>
-    <a href="<?php echo BASE_URL; ?>public/index.php" class="btn btn-primary">Go to Homepage</a>
-</div>
-<?php include(__DIR__.'/../includes/footer.php'); ?>
